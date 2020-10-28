57f86e253f64200d13bead8c55b62fe39ec21da5
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-10-28 08:46:13 -0400

    Fetch & Render posts: working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 // eslint-disable-next-line
 export default () => {
   return (
     <div className="container mt-4">
       <h1>Create Post</h1>
       <PostCreate />
+      <br />
+      <hr />
+      <h2> Posts </h2>
+      <PostList />
     </div>
   );
 };
